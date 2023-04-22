d8fe8349f69ac3a2a501e99a06e63f7aac984efe
Dan1ss1m0 <105284739+Dan1ss1m0@users.noreply.github.com>
2023-04-23 02:09:13 +0300

    Add files via upload

--- locator.h
+++ locator.h
@@ -3,30 +3,45 @@
 
 class ray{
 private:
+   float width = 1.5; //width of the ray in degrees in both sections
+   float Rmax = 10000;
    float phi;   //azimut
    float teta;  //elevation
 public:
+   position to_coordinates(float R); // (R, Teta, Phi) to (x, y, z)
    ray(float p, float t){
        this->phi = p;
        this->teta = t;
    }
-   virtual void update_angles(); //should be update_angles(targets_pool, &pbu);
+   position pos;
+   virtual void update_angles(vector <positon> targets, ControlCenter& PBU); //should be update_angles(targets_pool, &pbu);
    float get_teta();
    float get_phi();
 };
+
 class tracking_ray:public ray{
 public:
-    tracking_ray(int p, int t):ray(p,t){}
-    void update_angles();
+    float get_delta_phi();  //it CALCULATES delta_phi
+    float get_delta_teta();
+    tracking_ray(int p, int t, int target_id):ray(p,t){ this->target_id = target_id;}
+    void update_angles(vector <positons> targets, ControlCenter& PBU);
+private:
+    float p = 1;     //propoertional coefficent
+    float d = 0.01;  //differentional coefficient
+    float delta_phi = 0;
+    float delta_teta = 0;
+    int target_id;
 };
 
 class locator
 {
 public:
-    locator();
+    position pos;
+    locator(float x, float y, float z);
     void add_ray();
-    void del_ray(int i);
+    void del_ray(int n); //delete
 private:
+    int locator_id;
     ray* rays[10];
 };
 
